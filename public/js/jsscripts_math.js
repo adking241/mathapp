50cb1fe528de5243019d71268a9b23c6bb79bbaa
ANITA KING <ADKING>
2017-07-23 00:50:29 -0400

    css still needs some work

--- public/js/jsscripts_math.js
+++ public/js/jsscripts_math.js
@@ -2,7 +2,4 @@ function mathAlert() {
 	var numberOne = document.getElementById('num1').value;
 	var numberTwo = document.getElementById('num2').value;
 	var operator = document.getElementById('action').value;
-
-	alert(This is the math problem you are trying to solve: ' + numberOne + ' ' + operator + ' ' + numberTwo');
-
 }
