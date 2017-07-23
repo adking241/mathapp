766506d6c6f45e837c45c689fcc2c3ffc38bbd4c
ANITA KING <ADKING>
2017-07-22 23:34:30 -0400

    working math app w drop down list for operators

---
+++ public/js/jsscripts_math.js
@@ -0,0 +1,8 @@
+function mathAlert() {
+	var numberOne = document.getElementById('num1').value;
+	var numberTwo = document.getElementById('num2').value;
+	var operator = document.getElementById('action').value;
+
+	alert(This is the math problem you are trying to solve: ' + numberOne + ' ' + operator + ' ' + numberTwo');
+
+}
